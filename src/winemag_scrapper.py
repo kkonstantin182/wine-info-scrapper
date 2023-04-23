d87c4078d23eadd763a572a2f570c9af2559fc88
k.konstantin182 <k.konstantin182@yandex.ru>
2023-04-23 23:41:20 +0200

    Multiprocessig for review extraction added

--- src/winemag_scrapper.py
+++ src/winemag_scrapper.py
@@ -4,48 +4,102 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from random import randint
 from pathlib import Path 
-from configuration import FIRST_PAGE, BASE_URL, DATA_DIR, FILE_NAME_URLS, FILE_NAME_REVIEWS, HEADERS, DATA_DIR, FILE_NAME_URLS, FILE_NAME_REVIEWS
+from configuration import FIRST_PAGE, BASE_URL, DATA_DIR, FILE_NAME_URLS, FILE_NAME_REVIEWS, HEADERS, DATA_DIR, FILE_NAME_URLS, FILE_NAME_REVIEWS, MAX_RETRIES
 from configuration import get_project_root
+
+from multiprocessing import Pool
+from tqdm import tqdm
+import time
+
+
+# https://xang1234.github.io/multiprocessing/
+# ConnectionResetError: [WinError 10054]
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print("Operation is completed")
+        print(f"It took: {end_time - start_time:.5f} seconds")
+        return result
+    return wrapper
+
 
 
 class Scrapper:
 
-    def __init__(self, country):
+    def __init__(self, country, header=HEADERS, first_page=FIRST_PAGE):
 
         self.country = country
-        self.n_pages = None
-        self.first_page = FIRST_PAGE
-        self.init_url = BASE_URL.format(**{"country": self.country, "n_page": self.first_page})
-        self.total_reviews = self._get_total_reviews()
-        self.n_iter = round(self.total_reviews / 20) # 20 is the # of reviews per BASE_URL page (see winemag.com)
-        self.url_list = []
+        self._header = header
+        self.first_page = first_page
+        # self._init_url = BASE_URL.format(**{"country": self.country, "n_page": self.first_page})
+        self._init_url = BASE_URL
 
-        print(f"The total number of pages found is {self.n_iter}")
+        self.url_list = []
+        
+        self.total_reviews = self._get_total_reviews()
+        self._n_iter = round(self.total_reviews / 20) # 20 is the # of reviews per BASE_URL page (see winemag.com)
+        self._n_pages = 1
+        
+        
+        print(f"The total number of pages found is {self._n_iter}")
+
+    
+    @property
+    def n_pages(self):
+        return self._n_pages
+    
+    @n_pages.setter
+    def n_pages(self, n_pages):
+        if (n_pages >=1) & (n_pages <= self._n_iter):
+            self._n_pages = n_pages
+        else:
+            raise ValueError(f"The number of pages must be within the range [1, {self._n_iter}]")
+    
 
     def _get_total_reviews(self):
-        response = requests.get(url=self.init_url, headers=HEADERS)
+        response = requests.get(url=self._init_url.format(**{"country": self.country, "n_page": self.first_page}), headers=self._header)
         bs_obj = BeautifulSoup(response.text, 'html.parser')
         return int(bs_obj.find("span", {"class": "results-count"}).text.split("of")[1].strip().replace(",", ""))
     
-    def set_n_pages(self, n_pages):
-        self.n_pages = n_pages
-        
+
+    @timer
     def _get_reviews_urls(self):
-    
-        page = self.first_page
         
-        while page <= self.n_pages:
+        page = self.first_page
+        num_retries = 0
+        
+        with tqdm(total=self.n_pages) as pbar:
+            while page <= self.n_pages:
 
-            response = requests.get(url=self.init_url.format(**{"country": self.country, "n_page": page}), headers=HEADERS, timeout=randint(5,7))
-            soup = BeautifulSoup(response.text, 'html.parser')
-            review_urls = soup.find_all("li", {"class": "review-item"})
+                try:
+                    response = requests.get(url=self._init_url.format(**{"country": self.country, "n_page": page}), headers=self._header, timeout=randint(5,7))
+                    response.raise_for_status()
+                except (requests.exceptions.HTTPError, requests.exceptions.RequestException) as e:
+                    print(f"Request failed. Retrying ({num_retries+1}/{MAX_RETRIES})...")
+                    num_retries += 1
+                    if num_retries >= MAX_RETRIES:
+                        raise e
+                    time.sleep(randint(5, 15))
+                    continue
 
-            for url in review_urls:
-                review_url = url.find("a", {"class": "review-listing"})["href"]
-                self.url_list.append(review_url)
+                soup = BeautifulSoup(response.text, 'html.parser')
+                review_urls = soup.find_all("li", {"class": "review-item"})
+
+                for url in review_urls:
+                    review_url = url.find("a", {"class": "review-listing"})["href"]
+                    self.url_list.append(review_url)
+        
+                ###
+                print(page)
+                print(self._init_url.format(**{"country": self.country, "n_page": page}))
+                ###
                 
-            
-            page += 1
+                page += 1
+                
+                pbar.update(1)
 
         output_folder =  get_project_root() / DATA_DIR 
         output_folder.mkdir(exist_ok=True)
@@ -54,95 +108,86 @@ class Scrapper:
             for url in self.url_list:
                 f.write(f"{url}\n")
 
-        return self.url_list
+
+    def _extract_review(self, url):
+        retries = 0
+        while True:
+            try:
+                request = requests.get(url=url, headers=self._header, timeout=(randint(3, 5), randint(5, 7))) # For winemag.com read timout must be >= 5 
+                soup = BeautifulSoup(request.text, 'html.parser')
+
+                # Some data are stored in json
+                soup_json = soup.find_all("script", type="application/ld+json")[1]
+                json_data = json.loads(soup_json.string)
+
+                ###### Extract from json ######
+
+                # Wine name
+                wine_name = json_data["name"]
+                # Wine type
+                wine_type = json_data["category"]
+                # Reviewer's name
+                reviewer_name = json_data["review"]["author"]["name"]
+                # Review text
+                review_text = json_data["review"]["reviewBody"]
+                # Rating
+                rating = json_data["review"]["reviewRating"]["ratingValue"]
+
+                ###### Extract from html #####
+
+                # Find the place in the document ralated to the info needed
+                html_data = soup.find("ul", {"class": "primary-info"}).find_all("li", {"class": "row"})
+                # Extract price value
+                price = html_data[1].find_all("span")[1].find("span").text.split(",")[0]
+                # Extract Designation
+                designation = html_data[2].find_all("span")[1].text
+                # Extract variery (grape type)
+                grape_list = html_data[3].find_all("span")[1].text.split(",")
+                grape1 = html_data[3].find_all("span")[1].text.split(",")[0]
+                if len(grape_list) != 1:
+                    grape2 = html_data[3].find_all("span")[1].text.split(",")[1]
+                else: grape2 = None
+                # Extract winery name 
+                winery = html_data[5].find_all("span")[1].text
+
+                data = pd.DataFrame.from_dict(
+                    {
+                    "wine_name": [wine_name],
+                    "winery": [winery],
+                    "type": [wine_type],
+                    "price": [price],
+                    "designation": [designation],
+                    "variety1": [grape1],
+                    "variety2": [grape2], 
+                    "reviewer": [reviewer_name],
+                    "review" : [review_text],
+                    "rating": [rating]
+
+                }
+                )
+                return data
+            except (ConnectionError, requests.exceptions.Timeout) as e:
+                retries += 1
+                print(f"Connection failed, retrying ({retries}/{self.MAX_RETRIES})")
+                if retries == self.MAX_RETRIES:
+                    raise e
+                time.sleep(randint(5, 15))
+                continue
+            except Exception as e:
+                print(f"Unexpected error: {e}")
+                return None
     
+    
+    @timer
     def extract_reviews(self):
-            
-        results = pd.DataFrame(columns=[
-            'wine_name', 
-            'winery', 
-            'type', 
-            'price', 
-            'designation', 
-            'variety1', 
-            'variety2', 
-            'reviewer', 
-            'review', 
-            'rating'])
+        with Pool() as pool:
+            results = list(tqdm(pool.imap(self._extract_review, self.url_list), total=len(self.url_list)))
 
-        for url in self.url_list:
-
-            request = requests.get(url=url, headers=HEADERS, timeout=(randint(3, 5), randint(5, 7))) # For winemag.com read timout must be >= 5 
-            soup = BeautifulSoup(request.text, 'html.parser')
-
-
-            # Some data are stored in json
-            soup_json = soup.find_all("script", type="application/ld+json")[1]
-            json_data = json.loads(soup_json.string)
-
-            # __Extract from json__
-
-            # Wine name
-            wine_name = json_data["name"]
-
-            # Wine type
-            wine_type = json_data["category"]
-
-            # Reviewer's name
-            reviewer_name = json_data["review"]["author"]["name"]
-
-            # Review text
-            review_text = json_data["review"]["reviewBody"]
-            
-            # Rating
-            rating = json_data["review"]["reviewRating"]["ratingValue"]
-
-            # __Extract from html__
-
-            # Find the place in the document ralated to the info needed
-            html_data = soup.find("ul", {"class": "primary-info"}).find_all("li", {"class": "row"})
-
-            # Extract price value
-            price = html_data[1].find_all("span")[1].find("span").text.split(",")[0]
-            
-
-            # Extract Designation
-            designation = html_data[2].find_all("span")[1].text
-            
-            # Extract variery (grape type)
-            grape_list = html_data[3].find_all("span")[1].text.split(",")
-            
-            grape1 = html_data[3].find_all("span")[1].text.split(",")[0]
-            if len(grape_list) != 1:
-                grape2 = html_data[3].find_all("span")[1].text.split(",")[1]
-            else: grape2 = None
-
-            # Extract winery name 
-            winery = html_data[5].find_all("span")[1].text
-            
-            data = pd.DataFrame.from_dict(
-                {
-                "wine_name": [wine_name],
-                "winery": [winery],
-                "type": [wine_type],
-                "price": [price],
-                "designation": [designation],
-                "variety1": [grape1],
-                "variety2": [grape2], 
-                "reviewer": [reviewer_name],
-                "review" : [review_text],
-                "rating": [rating]
-
-            }
-            )
-
-            results = pd.concat([results, data], axis=0)
-
-        output_folder =  get_project_root() / DATA_DIR 
+        output_folder = get_project_root() / DATA_DIR 
         output_folder.mkdir(exist_ok=True)
-        results.to_csv(f"{FILE_NAME_REVIEWS}.csv")
+        results = pd.concat(results, axis=0)
+        results.to_csv(output_folder / FILE_NAME_REVIEWS)
+        print("Completed")
 
-
-   
-         
+    
 
